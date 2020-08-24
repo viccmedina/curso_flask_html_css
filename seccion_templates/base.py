3ae7fc09f3000f6a5a01670d48d135856847d3e5
viccmedina <medina.vicc@gmail.com>
2020-08-24 08:04:14 -0300

    control de flujo en los templates. for e if

--- seccion_templates/base.py
+++ seccion_templates/base.py
@@ -7,8 +7,10 @@ def index():
 	name = 'Vicky'
 	letters = list(name)
 	pup_dictionary = {'pup_name': 'Sammy'}
+	puppies_names = [ 'Rufus', 'Spike', 'Boby']
 	return render_template('basic.html', name=name, 
-		letters=letters, pup_dictionary=pup_dictionary)
+		letters=letters, pup_dictionary=pup_dictionary,
+		puppies_names=puppies_names)
 
 if __name__ == '__main__':
 	app.run(debug=True)
