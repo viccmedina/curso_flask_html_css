50ecdb958687b3e28deb54d32060d562eb8a7238
Victoria Medina <vmedina@msa.com.ar>
2020-08-29 20:38:26 -0300

    se actualizan los requerimientos, hacemos un primer enfoque sobre persistencia en db

---
+++ seccion_database/setupDataBase.py
@@ -0,0 +1,44 @@
+from basic import db, Puppy
+
+# Crea todas las tablas
+db.create_all()
+
+sam = Puppy('Sammy', 3)
+frank = Puppy('Frankie', 4)
+
+# Podemos agregarlo a la db con una sola línea
+# Para ello, le pasamos por parámetro una lista.
+
+db.session.add_all([sam, frank])
+
+# Guardamos los objetos
+db.session.commit()
+
+# Obtenemos todos los elementos de tipo Puppy
+# de la tabla puppy
+
+all_puppies = Puppy.query.all()
+print(all_puppies)
+
+# Podemos filtrar por una columna de la tabla
+# Siempre dará como resultado una lista.
+
+puppy_frankie = Puppy.query.filter_by(name='Frankie')
+
+########## UPDATE
+
+# Recuperamos el primer elemento
+first_puppy = Puppy.query.get(1)
+
+first_puppy.age = 10
+db.session.add(first_puppy)
+db.session.commit()
+
+###### DELETE
+
+second_puppy = Puppy.query.get(2)
+db.session.delete(second_puppy)
+db.session.commit()
+
+all_puppies = Puppy.query.all()
+print(all_puppies)
