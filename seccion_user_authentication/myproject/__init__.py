2ae1b1cb1e127f821eeb988909a58e3a32a6c410
viccmedina <medina.vicc@gmail.com>
2020-08-31 17:51:50 -0300

    primera parte de la autenticacion de usuarios, se creo estructura del proyecto

---
+++ seccion_user_authentication/myproject/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login iport LoginManager
+
+login_manager = LoginManager()
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/victoria/desarrollo/flask/seccion_user_authentication/myproject/data.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'mysecretkey'
+
+db = SQLAlchemy(app)
+Migrate(app)
+
+login_manager.init_app(app)
+login_manager.login_view = 'login'
