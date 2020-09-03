f5bea8758b5a316285b166294aeb4502d8b227eb
viccmedina <medina.vicc@gmail.com>
2020-09-02 23:28:02 -0300

    se agrega pagina de error y blueprint que la maneja

--- large_project/puppycompanyblog/__init__.py
+++ large_project/puppycompanyblog/__init__.py
@@ -2,4 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 
 from puppycompanyblog.core.views import core
+from puppycompanyblog.error_pages.handlers import error_pages
+
 app.register_blueprint(core)
+app.register_blueprint(error_pages)
